077c9348a8196e21099e2a7e6b2c5d52631dbfec
irenenguyen <mpn1008@gmail.com>
2025-03-06 14:44:13 +0100

    feat: kafka consumer

--- src/main/java/com/example/kafka_demo/infra/kafka/KafkaConfig.java
+++ src/main/java/com/example/kafka_demo/infra/kafka/KafkaConfig.java
@@ -1,27 +1,15 @@
-//package com.example.kafka_demo.infra.kafka;
-//
-//import lombok.Data;
-//import lombok.extern.slf4j.Slf4j;
-//import org.apache.kafka.clients.producer.ProducerConfig;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.kafka.core.KafkaTemplate;
-//import org.springframework.kafka.core.ProducerFactory;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//
-//@Data
-//@Configuration
-//@ConfigurationProperties("kafkademo.infra.kafka")
-//@Slf4j
-//public class KafkaConfig {
-//  private String topic;
-//
-//  @Bean
-//  public KafkaTemplate<String, Object> kafkaTemplate() {
-//    return new KafkaTemplate<String, Object>(producerFactory());
-//  }
-//}
+package com.example.kafka_demo.infra.kafka;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+
+@Data
+@Configuration
+@ConfigurationProperties("kafkademo.infra.kafka")
+@Slf4j
+public class KafkaConfig {
+  private String paymentEventTopicId = "payment-event";
+}
--- src/main/java/com/example/kafka_demo/infra/kafka/KafkaConsumer.java
+++ src/main/java/com/example/kafka_demo/infra/kafka/KafkaConsumer.java
@@ -1,10 +1,24 @@
 package com.example.kafka_demo.infra.kafka;
 
+import com.example.payment.PaymentEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class KafkaConsumer {
 
+  private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+  @KafkaListener(topics = "#{kafkaConfig.paymentEventTopicId}")
+  public void onBuyerCreatedEvent(
+      PaymentEvent event,
+      Acknowledgment acknowledgment) {
+    log.info("method=onBuyerCreatedEvent, eventId={}", event.getEventId());
+    acknowledgment.acknowledge();
+  }
 }
